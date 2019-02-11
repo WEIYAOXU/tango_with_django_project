bd281c1e9fe34147665de0f5d0ac6153729f096b
WEIYAO XU <2362356x@student.gla.ac.uk>
2019-02-11 07:19:14 +0000

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     llist_display = ('title', 'category', 'url')
@@ -9,6 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
+admin.site.register(UserProfile)
 
 
 # Register your models here.
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -107,6 +108,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+        'django.contrib.auth.hashers.BCryptPasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 
 
 # Internationalization
@@ -135,3 +144,4 @@ MEDIA_ROOT = MEDIA_DIR
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+LOGIN_URL = '/rango/login/'
